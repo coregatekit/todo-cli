777941a46523a0b4d20058fa4d43de1764a73125
Joe Kim <joesm.genie@gmail.com>
2026-03-30 18:58:37 +0700

    implement basic commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "todo")]
+#[command(about = "A simple CLI todo application", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Add { title: String },
+    List,    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Add { title } => {
+            println!("Adding todo: {}", title);
+            // Here you would add the logic to save the todo item
+        }
+        Commands::List => {
+            println!("Listing all todos...");
+            // Here you would add the logic to retrieve and display all todo items
+        }
+    }
 }
