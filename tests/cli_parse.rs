20b6b8a49e8832f0950e67eaecdd32805f1271d9
Joe Kim <joesm.genie@gmail.com>
2026-03-30 19:05:59 +0700

    test: run(add) returns Added line

--- tests/cli_parse.rs
+++ tests/cli_parse.rs
@@ -1,6 +1,14 @@
 use clap::Parser;
+use todo_cli::Cli;
 
 #[test]
 fn parse_add_command() {
     let _ = todo_cli::Cli::parse_from(["todo", "add", "Buy milk"]);
+}
+
+#[test]
+fn run_add_returns_added_line() {
+    let cli = Cli::parse_from(["todo", "add", "learn-rust"]);
+    let out = todo_cli::run(cli);    
+    assert!(out.contains("Added: learn rust\n"));
 }
