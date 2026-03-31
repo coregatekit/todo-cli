d80866f1260a38975fb9a9fc4aa7dec7f55536ba
Joe Kim <joesm.genie@gmail.com>
2026-03-31 10:11:30 +0700

    test: add/edit test for cli parse

--- tests/cli_parse.rs
+++ tests/cli_parse.rs
@@ -1,6 +1,24 @@
 use clap::Parser;
+use todo_cli::{Cli, Commands};
 
 #[test]
 fn parse_add_command() {
-    let _ = todo_cli::Cli::parse_from(["todo", "add", "Buy milk"]);
+    let cli = todo_cli::Cli::parse_from(["todo", "add", "learn-rust"]);
+    assert_eq!(cli.store.to_string_lossy(), "todo.json");
+
+    match cli.command {
+        Commands::Add { title } => assert_eq!(title, "learn-rust"),
+        _ => panic!("Expected Add command"),
+    }
+}
+
+#[test]
+fn parse_store_option() {
+    let cli = Cli::parse_from(["todo", "--store", "my.json", "list"]);
+    assert_eq!(cli.store.to_string_lossy(), "my.json");
+
+    match cli.command {
+        Commands::List => {},
+        _ => panic!("Expected List command"),
+    }
 }
