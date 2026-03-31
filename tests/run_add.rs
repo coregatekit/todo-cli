b0d3933f840a42b4b1d93e1ba59a8cfc946dde30
Joe Kim <joesm.genie@gmail.com>
2026-03-31 10:27:08 +0700

    test: fix old test cases to handle result

--- tests/run_add.rs
+++ tests/run_add.rs
@@ -5,5 +5,5 @@ use todo_cli::Cli;
 fn run_add_returns_added_line() {
     let cli = Cli::parse_from(["todo", "add", "learn-rust"]);
     let out = todo_cli::run(cli);
-    assert_eq!(out, "Added: learn-rust\n");
+    assert_eq!(out.unwrap(), "Added: learn-rust\n");
 }
--- tests/run_list.rs
+++ tests/run_list.rs
@@ -4,5 +4,5 @@ use clap::Parser;
 fn run_list_empty_returns_not_items() {
     let cli = todo_cli::Cli::parse_from(["todo", "list"]);
     let out = todo_cli::run(cli);
-    assert_eq!(out, "No items\n");
+    assert_eq!(out.unwrap(), "No items\n");
 }
